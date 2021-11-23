fb4d08f7b0a469960d9983d9239fd83e77d0409c
hyeongeonhwang <puheh@nate.com>
2021-11-24 00:31:46 +0900

    s3

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,113 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <h1>file upload</h1>
+    <input
+      id="file-selector"
+      ref="file"
+      type="file"
+      @change="handleFileUpload()"
+    />
+    <v-btn @click="upload" color="primary">upload</v-btn>
+
+    <div>
+      <li
+        v-for="(album, idx) in albumNameList"
+        :key="idx"
+        @click="viewAlbum(album)"
+      >
+        {{ album }}
+      </li>
+    </div>
+    <div>
+      <img
+        v-for="(imageUrl, idx) in albumImageList"
+        :key="idx"
+        style="width: 128px; height: 128px"
+        :src="imageUrl"
+      />
+    </div>
+  </v-container>
 </template>
 
 <script>
+import AWS from "aws-sdk";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      file: {
+        type: Object,
+      },
+      bucketName: "daou-emoticon-platform-eon",
+      region: "ap-northeast-2", // 리전
+      identityPoolId: "ap-northeast-2:3873425b-6259-466b-992a-d420a3d1b5a7",
+      s3: {
+        type: Object,
+      },
+      albumNameList: [],
+      albumImageList: [],
+    };
+  },
+  mounted() {
+    AWS.config.update({
+      region: this.region,
+      credentials: new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: this.identityPoolId,
+      }),
+    });
+    this.s3 = new AWS.S3({
+      apiVersion: "2006-03-01",
+      params: { Bucket: this.bucketName },
+    });
+    this.list();
+  },
+  methods: {
+    handleFileUpload() {
+      console.log("handle file");
+      this.file = this.$refs.file.files[0];
+    },
+    viewAlbum(albumName) {
+      const context = this;
+      this.s3.listObjects({ Prefix: albumName }, function (err, data) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        context.albumImageList = data.Contents.map((content) => {
+          const href = this.request.httpRequest.endpoint.href;
+          const bucketUrl = href + context.bucketName + "/";
+          return bucketUrl + content.Key;
+        }).filter((url) => url.charAt(url.length - 1) != "/");
+        console.log(context.albumImageList);
+      });
+    },
+    list() {
+      this.s3.listObjects({ Delimiter: "/" }, (err, data) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        this.albumNameList = data.CommonPrefixes.map((album) => album.Prefix);
+        console.log(this.albumNameList);
+      });
+    },
+    upload() {
+      const photoKey = "test2/" + this.file.name;
+      this.s3.upload(
+        {
+          Key: photoKey,
+          Body: this.file,
+          ACL: "public-read",
+        },
+        (err, data) => {
+          if (err) {
+            console.log(err);
+            return alert("there was an error: ", err.message);
+          }
+          alert("Success");
+          console.log(data);
+        }
+      );
+    },
+  },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
